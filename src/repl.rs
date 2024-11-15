4b202ab83e8549dfd033a8394b7243c54b581b97
DinnerWithoutTomato <xqiutongxue@foxmail.com>
2024-11-15 16:32:20 +0800

    refactor: refactor REPL
    重构REPL启动函数，将针对引擎的逻辑分离为`start_eval`和`_vm`立函数，简化代码结构。修正了错误机制，在解析程序出错时能输出错误信息

--- src/repl.rs
+++ src/repl.rs
@@ -16,44 +16,71 @@ const PROMPT: &str = ">> ";
 
 use std::io::Write;
 
-use crate::{compiler::Compiler, evaluator, lexer, object::Environment, parser, vm};
+use crate::{
+    ast::Program,
+    compiler::Compiler,
+    evaluator, lexer,
+    object::Environment,
+    parser::{self, ParseError},
+    vm,
+};
 
 pub enum Engine {
     Eval,
     VM,
 }
 
-pub fn start(stdin: std::io::Stdin, mut out: std::io::Stdout, engine: Engine) {
+pub fn start(stdin: std::io::Stdin, out: std::io::Stdout, engine: Engine) {
     println!("{MONKEY_FACE}");
+    match engine {
+        Engine::Eval => start_eval(stdin, out),
+        Engine::VM => start_vm(stdin, out),
+    }
+}
+
+fn start_eval(stdin: std::io::Stdin, mut out: std::io::Stdout) {
     let env = Environment::new().to_rc();
 
     loop {
-        out.write_all(PROMPT.as_bytes()).unwrap();
-        out.flush().unwrap();
-
-        let mut line = String::new();
-        stdin.read_line(&mut line).expect("Failed to read line");
-
-        let lexer = lexer::Lexer::new(line);
-        let mut parser = parser::Parser::new(lexer);
-
-        match parser.parse_program() {
-            Ok(program) => match engine {
-                Engine::Eval => {
-                    let evaluated = evaluator::eval(&program, env.clone());
-                    println!("{}", evaluated.inspect());
-                }
-                Engine::VM => {
-                    let mut comp = Compiler::new();
-                    comp.compile(&program).expect("Failed to compile program");
-                    let mut vm = vm::new(comp.byte_code());
-                    vm.run().expect("execute bytecode failed");
-
-                    let stack_top = vm.last_popped_elem().unwrap();
-                    println!("{}", stack_top.inspect())
-                }
-            },
+        match read_and_parse(&stdin, &mut out) {
+            Ok(program) => {
+                let evaluated = evaluator::eval(&program, env.clone());
+                println!("{}", evaluated.inspect());
+            }
             Err(error) => eprintln!("{}", error),
         }
     }
+}
+
+fn start_vm(stdin: std::io::Stdin, mut out: std::io::Stdout) {
+    loop {
+        match read_and_parse(&stdin, &mut out) {
+            Ok(program) => {
+                // FIXME: 全局变量/环境需要绑定
+                let mut comp = Compiler::new();
+                comp.compile(&program).expect("Failed to compile program");
+                let mut vm = vm::new(comp.byte_code());
+                vm.run().expect("execute bytecode failed");
+
+                let stack_top = vm.last_popped_elem().unwrap();
+                println!("{}", stack_top.inspect())
+            }
+            Err(error) => eprintln!("{}", error),
+        }
+    }
+}
+
+fn read_and_parse(
+    stdin: &std::io::Stdin,
+    out: &mut std::io::Stdout,
+) -> Result<Program, ParseError> {
+    out.write_all(PROMPT.as_bytes()).unwrap();
+    out.flush().unwrap();
+
+    let mut line = String::new();
+    stdin.read_line(&mut line).expect("Failed to read line");
+
+    let lexer = lexer::Lexer::new(line);
+    let mut parser = parser::Parser::new(lexer);
+    parser.parse_program()
 }
